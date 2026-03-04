240a0c2ac93f632cc58f5ceec4cf7fc96daa3713
DevWoojin97 <dnekdnwls@gmail.com>
2026-03-04 14:02:39 +0900

    하드코딩된 res.redirect 수정

--- src/config/env.js
+++ src/config/env.js
@@ -16,4 +16,5 @@ export const env = {
   PORT: process.env.PORT || 5050,
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
+  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
 };
